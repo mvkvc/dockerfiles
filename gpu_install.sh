a9950db25a379841f683e659fcb965cc4a7dc693
Marko Vukovic <mvukovic.cloud@gmail.com>
2020-12-21 04:34:01 -0500

    Working now

--- gpu_install.sh
+++ gpu_install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# Install NVIDIA drivers
+# Add repo and update
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+apt-get update
+
+# Install nvidia driver
 apt-get install ubuntu-drivers-common -y
 ubuntu-drivers devices
-ubuntu-drivers autoinstall
+apt-get install nvidia-driver-450 -y
 
 # Install docker
 curl https://get.docker.com | sh \
@@ -11,9 +17,5 @@ curl https://get.docker.com | sh \
   && systemctl enable docker
 
 # Install nvidia-docker
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-apt-get update
 apt-get install -y nvidia-docker2
 systemctl restart docker
